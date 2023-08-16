945f087dabe6ce000ebc45d2eeb6f4c8f177542a
unknown <nicholaskirui6@gmail.com>
2023-08-16 21:05:49 +0300

    login and sign-up

---
+++ wp-content/themes/helpdesk/dashboard.php
@@ -0,0 +1,7 @@
+<?php
+/*
+Template Name: Dashboard Page
+*/
+?>
+
+<?php get_sidebar() ?>
--- wp-content/themes/helpdesk/footer.php
+++
@@ -1,7 +0,0 @@
-<div>
-    <footer>
-    &copy; 2023 Helpdesk Project by Nicholas Kirui. All rights reserved.
-    </footer>
-</div>
-</body>
-</html>
