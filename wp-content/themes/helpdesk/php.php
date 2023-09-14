180b8b3ffcfb8935d0ac75ef32038b60063a7612
unknown <nicholaskirui6@gmail.com>
2023-09-14 18:13:32 +0300

    php

---
+++ wp-content/themes/helpdesk/php.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+<?php 
+echo'hello world';
+echo'<h1>Nick Site</h1>';
+echo '<hr>';
+
+$name = 'Nicholas';
+$age = '28';
+echo 'There was once a man named $name <br> ';
+echo 'He was 28 years old <br> ';
+echo 'He really liked the name Nicholas <br> ';
+echo 'But didnt like being 28 <br>';
+?>
+
+</body>
+</html>
