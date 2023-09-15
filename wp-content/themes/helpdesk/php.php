fcc5a8b9731335648859ceffb021adfe684c0681
unknown <nicholaskirui6@gmail.com>
2023-09-15 14:32:35 +0300

    data

--- wp-content/themes/helpdesk/php.php
+++ wp-content/themes/helpdesk/php.php
@@ -7,17 +7,37 @@
 </head>
 <body>
 <?php 
-echo'hello world';
-echo'<h1>Nick Site</h1>';
-echo '<hr>';
+// initializing a program in php using echo command
+echo"hello world";
+echo"<h1>Nicholas Site</h1>";
+echo "<hr>";
 
-$name = 'Nicholas';
-$age = '28';
-echo 'There was once a man named $name <br> ';
-echo 'He was 28 years old <br> ';
-echo 'He really liked the name Nicholas <br> ';
-echo 'But didnt like being 28 <br>';
+// storing data using variables
+$name = "Nicholas";
+$age = "28";
+echo "There was once a man named $name <br>";
+echo "He was $age years old <br>";
+echo "He really liked the name $name <br>";
+echo "But didnt like being $age <br>";
+
+// data types in php
+// 1. string
+$phrase = "To be or not to be";
+// 2. integer 
+$age = 30;
+// 3. float
+$gpa = 4.56;
+// 4. boolean
+$ismale = false;
+// 5. no value
+null
 ?>
 
+<?php
+// you dont always have to store the data in variables
+echo "To be or not to be <br> ";
+echo "30 <br> ";
+echo 4.56;
+?>
 </body>
 </html>
